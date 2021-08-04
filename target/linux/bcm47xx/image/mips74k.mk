3e1c9a0ee1b19a884603fa83eb9629c586e4f7ce
alpha2x <hexagonwin@disroot.org>
2021-08-04 21:24:24 +0900

    Edit MIPS74K - ipTIME N704BCM

--- target/linux/bcm47xx/image/mips74k.mk
+++ target/linux/bcm47xx/image/mips74k.mk
@@ -497,10 +497,9 @@ TARGET_DEVICES += netgear_wnr3500-v2-vc
 define Device/iptime_n704bcm
   DEVICE_VENDOR := ipTIME
   DEVICE_MODEL := N704BCM
+  SUPPORTED_DEVICES := 0x058e:360
   DEVICE_PACKAGES := -wpad-basic-wolfssl
   IMAGE_SIZE := 3904k
-  KERNEL_INITRAMFS := kernel-bin | trx | iptime-brcm n704bcm | \
-        pad-extra-header 128k
   IMAGE/trx := append-rootfs | trx-without-jffs2 | iptime-brcm n704bcm | \
         pad-rootfs | append-metadata | check-size
 endef
